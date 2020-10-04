02ad34be37cf1083d2c6041bfad0f53fa0463edf
M AMAN CHAUDHRY <chaudhrya12@gmail.com>
2020-10-04 20:41:31 +0530

    first repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
+import Particles from 'react-particles-js';
+//import particlesconfig from './particlesconfig.json'
 import './App.css';
+import Clarifai from 'clarifai';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const app = new Clarifai.App({
+ apiKey: "4da8f62bc6f84716842e039e67265bbd",
+});
+
+
+const particlesConfiguration = {
+	  particles: { 	
+        number: {
+        	value: 180,
+        	density: {
+        		enable: true,
+        		value_area: 750
+        	}
+        },
+	  interactivity: {
+	  	detect_on: 'canvas',
+	  	events: {
+	  		onclick: {
+	  		enable: true,
+	  		mode: 'repulse'
+	  		},
+	  		onhover:{
+	  		enable: true,
+	  		mode: 'grab'
+	  		},
+	  		resize: true
+	  	},
+	  	modes: {
+	  		grab: {
+	  			distance: 400,
+	  			line_linked: {
+	  				opacity: 1
+	  			}
+	  		},
+	  		
+	  		repulse: {
+	  			distance: 200,
+	  			duration: 0.4
+	  		}
+	  	},
+	  }
+	},
+	retina_detect: true
+}
+
+class App extends React.Component {
+	constructor() {
+		super ();
+		this.state = {
+			input: '',
+			imageUrl: '',
+			box: {}
+		}
+	}
+
+	onInputChange = (event) => {
+		this.setState({input: event.target.value});
+	}
+
+	onButtonSubmit = () => {
+ 			this.setState({imageUrl: this.state.input})
+ 				app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+				.then((response) => {
+ 					console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+					})
+					.catch((err) => {
+ 					console.log(err);
+					});
+};
+
+    render() {
+  	  return (
+    	<div className="App">
+      		<Particles className='particles'
+            	params={particlesConfiguration} 
+            />
+      		<Navigation />
+      		<Logo />
+      		<Rank />
+      		<ImageLinkForm 
+      		 onInputChange={this.onInputChange} 
+      		 onButtonSubmit={this.onButtonSubmit}
+      		/>
+      		<FaceRecognition imageUrl={this.state.imageUrl}/>
+    	</div>
   );
+  }
 }
 
 export default App;
